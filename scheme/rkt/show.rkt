372b3b80abb87c3a2e674ec4c2edbea9cc51af1d
Sergi Pasoev <s.pasoev@gmail.com>
2014-09-16 16:04:35 +0400

    slides

---
+++ scheme/rkt/show.rkt
@@ -0,0 +1,13 @@
+#lang slideshow
+
+;first slide
+(slide 
+ #:title "Slide programming"
+ (t "Hello, World")
+       (t "Click to go to the next slide")
+       (t "Or press n"))
+
+; second slide
+(slide
+ (t "This is the second slide"))
+
